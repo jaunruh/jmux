bfb07116a75f2af5c87087a542684810201ea8d3
Johannes Unruh <johannes@unruh.ai>
2025-07-02 13:56:53 +0200

    Updated Tests

--- test/test_json_demux.py
+++ test/test_json_demux.py
@@ -1,3 +1,7 @@
+import asyncio
+from asyncio import gather
+from typing import List
+
 import pytest
 from jmux.json_demux import SCityName, StreamingJSONSplitter
 
@@ -17,30 +21,228 @@ class AsyncStreamGenerator:
 
 
 @pytest.mark.parametrize(
-    "stream",
+    "stream,expected_operations",
     [
-        '{"city_name":"Paris","country":"France"}',
-        '{"city_name": "Paris", "country":"France"}',
-        '{\n\t"city_name": "Paris",\n\t"country": "France"\n}',
+        (
+            '{"city_name":"Paris","country":"France"}',
+            [
+                "[producer] sending: {",
+                '[producer] sending: "',
+                "[producer] sending: c",
+                "[producer] sending: i",
+                "[producer] sending: t",
+                "[producer] sending: y",
+                "[producer] sending: _",
+                "[producer] sending: n",
+                "[producer] sending: a",
+                "[producer] sending: m",
+                "[producer] sending: e",
+                '[producer] sending: "',
+                "[producer] sending: :",
+                '[producer] sending: "',
+                "[producer] sending: P",
+                "[city] received: P",
+                "[producer] sending: a",
+                "[city] received: a",
+                "[producer] sending: r",
+                "[city] received: r",
+                "[producer] sending: i",
+                "[city] received: i",
+                "[producer] sending: s",
+                "[city] received: s",
+                '[producer] sending: "',
+                "[producer] sending: ,",
+                '[producer] sending: "',
+                "[producer] sending: c",
+                "[producer] sending: o",
+                "[producer] sending: u",
+                "[producer] sending: n",
+                "[producer] sending: t",
+                "[producer] sending: r",
+                "[producer] sending: y",
+                '[producer] sending: "',
+                "[producer] sending: :",
+                '[producer] sending: "',
+                "[producer] sending: F",
+                "[country] received: F",
+                "[producer] sending: r",
+                "[country] received: r",
+                "[producer] sending: a",
+                "[country] received: a",
+                "[producer] sending: n",
+                "[country] received: n",
+                "[producer] sending: c",
+                "[country] received: c",
+                "[producer] sending: e",
+                "[country] received: e",
+                '[producer] sending: "',
+                "[producer] sending: }",
+            ],
+        ),
+        (
+            '{"city_name": "Paris", "country": "France"}',
+            [
+                "[producer] sending: {",
+                '[producer] sending: "',
+                "[producer] sending: c",
+                "[producer] sending: i",
+                "[producer] sending: t",
+                "[producer] sending: y",
+                "[producer] sending: _",
+                "[producer] sending: n",
+                "[producer] sending: a",
+                "[producer] sending: m",
+                "[producer] sending: e",
+                '[producer] sending: "',
+                "[producer] sending: :",
+                "[producer] sending:  ",
+                '[producer] sending: "',
+                "[producer] sending: P",
+                "[city] received: P",
+                "[producer] sending: a",
+                "[city] received: a",
+                "[producer] sending: r",
+                "[city] received: r",
+                "[producer] sending: i",
+                "[city] received: i",
+                "[producer] sending: s",
+                "[city] received: s",
+                '[producer] sending: "',
+                "[producer] sending: ,",
+                "[producer] sending:  ",
+                '[producer] sending: "',
+                "[producer] sending: c",
+                "[producer] sending: o",
+                "[producer] sending: u",
+                "[producer] sending: n",
+                "[producer] sending: t",
+                "[producer] sending: r",
+                "[producer] sending: y",
+                '[producer] sending: "',
+                "[producer] sending: :",
+                "[producer] sending:  ",
+                '[producer] sending: "',
+                "[producer] sending: F",
+                "[country] received: F",
+                "[producer] sending: r",
+                "[country] received: r",
+                "[producer] sending: a",
+                "[country] received: a",
+                "[producer] sending: n",
+                "[country] received: n",
+                "[producer] sending: c",
+                "[country] received: c",
+                "[producer] sending: e",
+                "[country] received: e",
+                '[producer] sending: "',
+                "[producer] sending: }",
+            ],
+        ),
+        (
+            '{\n\t"city_name": "Paris",\n\t"country": "France"\n}',
+            [
+                "[producer] sending: {",
+                "[producer] sending: \n",
+                "[producer] sending: \t",
+                '[producer] sending: "',
+                "[producer] sending: c",
+                "[producer] sending: i",
+                "[producer] sending: t",
+                "[producer] sending: y",
+                "[producer] sending: _",
+                "[producer] sending: n",
+                "[producer] sending: a",
+                "[producer] sending: m",
+                "[producer] sending: e",
+                '[producer] sending: "',
+                "[producer] sending: :",
+                "[producer] sending:  ",
+                '[producer] sending: "',
+                "[producer] sending: P",
+                "[city] received: P",
+                "[producer] sending: a",
+                "[city] received: a",
+                "[producer] sending: r",
+                "[city] received: r",
+                "[producer] sending: i",
+                "[city] received: i",
+                "[producer] sending: s",
+                "[city] received: s",
+                '[producer] sending: "',
+                "[producer] sending: ,",
+                "[producer] sending: \n",
+                "[producer] sending: \t",
+                '[producer] sending: "',
+                "[producer] sending: c",
+                "[producer] sending: o",
+                "[producer] sending: u",
+                "[producer] sending: n",
+                "[producer] sending: t",
+                "[producer] sending: r",
+                "[producer] sending: y",
+                '[producer] sending: "',
+                "[producer] sending: :",
+                "[producer] sending:  ",
+                '[producer] sending: "',
+                "[producer] sending: F",
+                "[country] received: F",
+                "[producer] sending: r",
+                "[country] received: r",
+                "[producer] sending: a",
+                "[country] received: a",
+                "[producer] sending: n",
+                "[country] received: n",
+                "[producer] sending: c",
+                "[country] received: c",
+                "[producer] sending: e",
+                "[country] received: e",
+                '[producer] sending: "',
+                "[producer] sending: \n",
+                "[producer] sending: }",
+            ],
+        ),
     ],
 )
 @pytest.mark.anyio
-async def test_json_demux(stream):
-    print("Testing JSON demux with stream:", stream)
-
+async def test_json_demux(stream: str, expected_operations: List[str]):
     llm_stream = AsyncStreamGenerator(stream)
     sCity = SCityName()
-    stream_wrapper = StreamingJSONSplitter(sCity)
-
-    async for char in llm_stream:
-        await stream_wrapper.feed_char(char)
+    splitter = StreamingJSONSplitter(sCity)
 
     city_name = ""
     country = ""
-    async for char in sCity.city_name:
-        city_name += char
-    async for char in sCity.country:
-        country += char
+    operation_list = []
 
-    assert city_name == "Paris", "Stream result does not match the input stream"
-    assert country == "France", "Stream result does not match the input stream"
+    async def consume_city():
+        nonlocal city_name
+        async for ch in sCity.city_name:
+            op = f"[city] received: {ch}"
+            operation_list.append(op)
+            city_name += ch
+
+    async def consume_country():
+        nonlocal country
+        async for ch in sCity.country:
+            op = f"[country] received: {ch}"
+            operation_list.append(op)
+            country += ch
+
+    async def produce():
+        async for ch in llm_stream:
+            op = f"[producer] sending: {ch}"
+            operation_list.append(op)
+            await splitter.feed_char(ch)
+            # Yield control to allow other tasks to run
+            # Necessary in the tests only, for API calls this is not needed
+            await asyncio.sleep(0)
+
+    await gather(
+        produce(),
+        consume_city(),
+        consume_country(),
+    )
+
+    assert city_name == "Paris"
+    assert country == "France"
+
+    assert operation_list == expected_operations
